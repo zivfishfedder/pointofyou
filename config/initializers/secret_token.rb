977f99b5d5f0ccc418b7110a2abccc8e2bab9799
zivfishfedder <fishfederationz@gmail.com>
2013-05-05 21:38:47 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pointofyou::Application.config.secret_token = '9c095fc06d31e9d025b64ccc52d6444e11b77e1a28747191cd2168dfd158ceb140f10499256345bf12248168da43917de41ac2eb91a23df839ef8dd5ce5ab7d1'
